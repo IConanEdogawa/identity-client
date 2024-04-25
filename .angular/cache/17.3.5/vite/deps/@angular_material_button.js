09de7561a210bb76d591b339f1e5320ee57d5b6f
IConanEdogawa <160710144+IConanEdogawa@users.noreply.github.com>
2024-04-25 10:06:00 +0500

    Add project files.

---
+++ .angular/cache/17.3.5/vite/deps/@angular_material_button.js
@@ -0,0 +1,34 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-FFTZQLSM.js";
+import "./chunk-B33W5ZKM.js";
+import "./chunk-26QCYA3A.js";
+import "./chunk-L4BWLG6A.js";
+import "./chunk-WSA2QMXP.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, inject } from '@angular/core';
+import { LoginRequest } from '../interfaces/login-request';
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { LoginResponse } from '../interfaces/login-response';
+import { Router } from '@angular/router';
+import { RegisterRequest } from '../interfaces/register-request';
+import { RegisterResponse } from '../interfaces/register-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  
+  constructor(private http:HttpClient) { }
+  apiUrl = environment.apiUrl;
+  tokenKey:string = 'token';
+  router = inject(Router)
+  
+  register(_data1:RegisterRequest) :Observable<RegisterResponse>{
+    const response =  this.http.post<RegisterResponse>(`${this.apiUrl}Users/Register`,_data1);
+    this.router.navigate(['/home'])
+    return response;
+
+  }
+
+  login(data: LoginRequest): Observable<LoginResponse>{
+    return this.http.post<LoginResponse>(`${this.apiUrl}Users/Login`, data).pipe(
+      map((response)=>{
+        if(response.isSuccess){
+          //localStorage.clear();
+          localStorage.setItem(this.tokenKey, response.token)
+        }
+        this.router.navigate(['/home'])
+        return response
+      })
+    );
+  }
+  
+}
